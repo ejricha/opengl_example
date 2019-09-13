f213d11faee7d2ef01fb8dd3a938380a35c0ad83
Eric Richardson <ejricha+github@gmail.com>
2019-09-12 22:10:20 -0400

    Add GLEW package, and fix GLFW

--- cmake/OpenGL.cmake
+++ cmake/OpenGL.cmake
@@ -12,9 +12,15 @@ include_directories(${OPENGL_INCLUDE_DIRS} ${GLUT_INCLUDE_DIRS})
 #find_package(GLM REQUIRED)
 #message(STATUS "GLM included at ${GLM_INCLUDE_DIR}")
 
+# GLEW
+find_package(GLEW REQUIRED)
+#message(STATUS "Found GLEW in ${GLEW_INCLUDE_DIR}")
+#include_directories(${GLEW_INCLUDE_DIR})
+
 # GLFW
-#find_package(GLFW3 REQUIRED)
-#message(STATUS "Found GLFW3 in ${GLFW3_INCLUDE_DIR}")
+find_package(glfw3 REQUIRED)
+message(STATUS "Found GLFW in ${GLFW_INCLUDE_DIR}")
+include_directories(${GLFW_INCLUDE_DIR})
 
 # ASSIMP
 #find_package(ASSIMP REQUIRED)
