acfbb5c250722548745b6f53afa624d69889ef44
Eric Richardson <ejricha+github@gmail.com>
2019-09-14 15:25:54 -0400

    Simplify the find_package calls

--- cmake/OpenGL.cmake
+++ cmake/OpenGL.cmake
@@ -2,26 +2,15 @@
 #
 # Find packages related to OpenGL
 
-# OpenGL and GLUT
+# OpenGL
 set(OpenGL_GL_PREFERENCE GLVND)
 find_package(OpenGL REQUIRED)
-find_package(GLUT REQUIRED)
-include_directories(${OPENGL_INCLUDE_DIRS} ${GLUT_INCLUDE_DIRS})
 
-# GLM
-#find_package(GLM REQUIRED)
-#message(STATUS "GLM included at ${GLM_INCLUDE_DIR}")
+# GLUT
+find_package(GLUT REQUIRED)
 
 # GLEW
 find_package(GLEW REQUIRED)
-#message(STATUS "Found GLEW in ${GLEW_INCLUDE_DIR}")
-#include_directories(${GLEW_INCLUDE_DIR})
 
-# GLFW
+# GLFW3
 find_package(glfw3 REQUIRED)
-message(STATUS "Found GLFW in ${GLFW_INCLUDE_DIR}")
-include_directories(${GLFW_INCLUDE_DIR})
-
-# ASSIMP
-#find_package(ASSIMP REQUIRED)
-#message(STATUS "Found ASSIMP in ${ASSIMP_INCLUDE_DIR}")
