64b16f134f864556f21ca9d538d3d24361b2971f
Eric Richardson <ejricha+github@gmail.com>
2019-09-18 20:58:48 -0400

    Use a macro add_executable_cpp() that will automatically target the correct libraries

--- cmake/OpenGL.cmake
+++ cmake/OpenGL.cmake
@@ -8,3 +8,14 @@ find_package(OpenGL REQUIRED)
 
 # GLFW3
 find_package(glfw3 REQUIRED)
+
+# Macro to add a simple executable
+macro(add_executable_cpp NAME)
+	set(BIN "${NAME}.bin")
+	set(CPP "${NAME}.cpp")
+	message("Building ${BIN} from ${CPP}")
+	add_executable(${BIN} ${CPP})
+	
+	# Be sure to link against OpenGL etc.
+	target_link_libraries(${BIN} ${OPENGL_LIBRARIES} glad glfw ${CMAKE_DL_LIBS})
+endmacro()
--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Compile using ninja in build_ninja
-ninja -C build_ninja ImageTest.bin
+ninja -C build_ninja
