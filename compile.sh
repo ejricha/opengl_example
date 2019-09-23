34dddc00062fcb283c73a73113ac7a405829333c
Eric Richardson <ejricha+github@gmail.com>
2019-09-23 19:20:48 -0400

    Compile using debug

--- compile.sh
+++ compile.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-# Compile using ninja in build_ninja
-ninja -C build_ninja
+# Compile with debug using ninja
+D=build_debug_ninja
+#mkdir -p $D
+#cd $D
+#cmake -GNinja -DCMAKE_BUILD_TYPE=Debug ..
+#cd ..
+ninja -C $D
